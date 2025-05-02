5aad25874b6a9075cf7a4ab1f593b00cb7f1f18a
Kyler Smith <143373950+Kbsmitty@users.noreply.github.com>
2025-05-01 18:54:52 -0600

    Bytecode 30/32 working

--- src/main/java/edu/montana/csci/csci468/parser/expressions/StringLiteralExpression.java
+++ src/main/java/edu/montana/csci/csci468/parser/expressions/StringLiteralExpression.java
@@ -40,8 +40,6 @@ public class StringLiteralExpression extends Expression {
 
     @Override
     public void compile(ByteCodeGenerator code) {
-        super.compile(code);
+        code.pushConstantOntoStack(stringValue);
     }
-
-
 }
