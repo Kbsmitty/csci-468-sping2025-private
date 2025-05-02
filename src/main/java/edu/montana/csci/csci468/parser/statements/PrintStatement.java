a30d59285dd9de7eaacafbc94e44f816c8b3ee4f
Kyler Smith <143373950+Kbsmitty@users.noreply.github.com>
2025-05-01 19:08:20 -0600

    Bytecode 30/32 working

--- src/main/java/edu/montana/csci/csci468/parser/statements/PrintStatement.java
+++ src/main/java/edu/montana/csci/csci468/parser/statements/PrintStatement.java
@@ -4,6 +4,7 @@ import edu.montana.csci.csci468.bytecode.ByteCodeGenerator;
 import edu.montana.csci.csci468.eval.CatscriptRuntime;
 import edu.montana.csci.csci468.parser.SymbolTable;
 import edu.montana.csci.csci468.parser.expressions.Expression;
+import org.objectweb.asm.Opcodes;
 
 public class PrintStatement extends Statement {
     private Expression expression;
@@ -11,7 +12,6 @@ public class PrintStatement extends Statement {
     public void setExpression(Expression parseExpression) {
         this.expression = addChild(parseExpression);
     }
-
 
     public Expression getExpression() {
         return expression;
@@ -37,7 +37,10 @@ public class PrintStatement extends Statement {
 
     @Override
     public void compile(ByteCodeGenerator code) {
-        super.compile(code);
+        code.addVarInstruction(Opcodes.ALOAD, 0);
+        getExpression().compile(code);
+        box(code, getExpression().getType());
+        code.addMethodInstruction(Opcodes.INVOKEVIRTUAL, code.getProgramInternalName(),
+                "print", "(Ljava/lang/Object;)V");
     }
-
 }
